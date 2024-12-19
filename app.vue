ce8f412415f818e745cd77f44573cd75e7ad227e
Justin <justin.bourrousse.ext@louisvuitton.com>
2024-12-19 17:03:31 +0100

    memleak repro

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+// persistent duplicated public apiKey in memory, one by code execution
+console.log(config)
+
+// no duplicates
+console.log('COUCOU')
+</script>
